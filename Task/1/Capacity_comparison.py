74b66a123f525d0ca7222423f1f762982222c80c
Bernardo <krunz04@gmail.com>
2020-10-27 17:14:35 +0100

    com

--- Task/1/Capacity_comparison.py
+++ Task/1/Capacity_comparison.py
@@ -3,9 +3,10 @@ import matplotlib.pyplot as plt
 
 
 
-C = [10,15,17,20,30,40,50]
-C_Splittable = [ 99999, 174.13961461928812, 157.9048878065381, 155.51358363486762, 155.51358363486762, 155.51358363486762, 155.51358363486762]
-C_Unsplittable = [99999, 186.5329006995025, 162.687496149879,  155.51358363486762, 155.51358363486762, 155.51358363486762, 155.51358363486762]
+C = [10,15,17,18,19,20,30,40,50]
+C_Splittable =   [ 99999, 174.13961461928812, 157.9048878065381, 155.51358363486762, 155.51358363486762, 155.51358363486762, 155.51358363486762, 155.51358363486762, 155.51358363486762]
+C_Unsplittable = [ 99999, 99999,              99999,             99999,              194.69352969073324, 189.40312010486429, 155.51358363486762, 155.51358363486762, 155.51358363486762]
+
 
 
 fig = plt.figure()
@@ -13,7 +14,7 @@ font = {'family' : 'sans', 'size'   : 12}
 plt.rc('font', **font)
 
 plt.plot(C[1:], C_Splittable[1:], 'bo-', color='red', label='Splittable')
-plt.plot(C[1:], C_Unsplittable[1:], 'bo-', color='blue', label='Unsplittable')
+plt.plot(C[4:], C_Unsplittable[4:], 'bo-', color='blue', label='Unsplittable')
 #plt.yscale('log')
 plt.title('CAPACITY COMPARISON')
 plt.xlabel('Link Capacity')
@@ -27,17 +28,17 @@ for x_,y_ in zip(C[1:],C_Splittable[1:]):
                  textcoords="offset points", # how to position the text
                  xytext=(0,10), # distance from text to points (x,y)
                  ha='center',
-                 rotation = 0,
+                 rotation = 45,
                  fontsize = 8) # horizontal alignment can be left, right or center
 
-for x_,y_ in zip(C[1:],C_Unsplittable[1:]):
+for x_,y_ in zip(C[4:],C_Unsplittable[4:]):
     label = round(y_, 2)
     plt.annotate(label, # this is the text
                  (x_,y_), # this is the point to label
                  textcoords="offset points", # how to position the text
                  xytext=(0,10), # distance from text to points (x,y)
                  ha='center',
-                 rotation = 0,
+                 rotation = 45,
                  fontsize = 8) # horizontal alignment can be left, right or center
 
 
